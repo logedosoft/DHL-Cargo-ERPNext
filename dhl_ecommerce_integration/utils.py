29e6ad627947bd19f762afdaf1a69ea66feaaf47
Turker TUNALI <turker.tunali@gmail.com>
2026-05-21 13:27:11 +0000

    cont: Login tests.

--- dhl_ecommerce_integration/utils.py
+++ dhl_ecommerce_integration/utils.py
@@ -1,15 +1,56 @@
 # Copyright (c) 2026, Logedosoft Business Solutions and contributors
 # For license information, please see license.txt
 
-import frappe, json
+import frappe, json, requests
+from datetime import datetime
 from frappe import msgprint, _
 
 def get_token():
-	dctResult = frappe._dict({
-		"op_result": False,
-		"op_message": ""
+	docDHLSettings = frappe.get_single("DHL Cargo Settings")
+	strTokenURL = docDHLSettings.web_service_url + "/mngapi/api/token"
+
+	dctPayload = {
+		"customerNumber": docDHLSettings.customer_number,
+		"password": docDHLSettings.get_password("password"),
+		"identityType": 1
 	}
-	
+
+	dctHeaders = {
+		"x-ibm-client-id": docDHLSettings.client_id,
+		"x-ibm-client-secret": docDHLSettings.get_password("client_secret"),
+		"Content-Type": "application/json"
+	}
+
+	try:
+		response = requests.post(strTokenURL, json=dctPayload, headers=dctHeaders, timeout=30)
+		dctResponse = response.json()
+		strJWT = dctResponse.get("jwt")
+
+		if strJWT:
+			docDHLSettings.jwt_token = strJWT
+			docDHLSettings.refresh_token = dctResponse.get("refreshToken")
+			strExpireDate = dctResponse.get("jwtExpireDate")
+			if strExpireDate:
+				docDHLSettings.jwt_expire_date = datetime.strptime(strExpireDate, "%d.%m.%Y %H:%M:%S")
+			docDHLSettings.save(ignore_permissions=True)
+			dctResult = frappe._dict({
+				"op_result": True,
+				"op_message": ""
+			})
+		else:
+			strHTTPMessage = dctResponse.get("httpMessage", "")
+			strMoreInfo = dctResponse.get("moreInformation", "")
+			strErrorMessage = " ".join(filter(None, [strHTTPMessage, strMoreInfo]))
+			dctResult = frappe._dict({
+				"op_result": False,
+				"op_message": strErrorMessage
+			})
+	except Exception:
+		frappe.log_error("DHL Get Token Error", frappe.get_traceback())
+		dctResult = frappe._dict({
+			"op_result": False,
+			"op_message": "Failed to obtain token — check Error Log for details"
+		})
 
 	return dctResult
 
