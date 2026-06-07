e3e46a7a80ddfec38189bec2d53d092b7596e4c5
Türker Tunalı <turker.tunali@gmail.com>
2026-06-07 15:52:30 +0300

    fix: add frappe.db.commit() after install setup in patch

--- dhl_ecommerce_integration/patches/setup_dhl_integration.py
+++ dhl_ecommerce_integration/patches/setup_dhl_integration.py
@@ -1,7 +1,11 @@
 # Copyright (c) 2026, Logedosoft Business Solutions and contributors
 # For license information, please see license.txt
 
+import frappe
+
 
 def execute():
     from dhl_ecommerce_integration.install import run_install_setup
+
     run_install_setup()
+    frappe.db.commit()
